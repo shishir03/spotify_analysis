7e47ec0a6ed510896c68822e8c4eaa6c690002ff
Shishir Iyer <siyer016@student.fuhsd.org>
2022-08-25 15:44:09 -0700

    Added promises

--- public/main.js
+++ public/main.js
@@ -1,37 +1,39 @@
 import { error, access_token } from "./params.js";
 import { getFeatures } from "./song_info.js";
 
-if(error) {
-    alert("There was an error during authentication");
-} else {
-    if(access_token) {
-        $.ajax({
-            url: 'https://api.spotify.com/v1/me/top/tracks?limit=50',
-            headers: {
-                'Authorization': 'Bearer ' + access_token
-            },
-            success: function(response) {
-                var items = response.items;
-                items.forEach(element => {
-                    var trackId = element.uri.substring(14);
-                    var features = getFeatures(trackId);
-
-                    var tag = $("<tr></tr>");
-                    var songTitle = $("<td></td>").append(features.title);
-                    var danceability = $("<td></td>").append(features.danceability);
-                    var energy = $("<td></td>").append(features.energy);
-                    var valence = $("<td></td>").append(features.valence);
-                    
-                    tag.append(songTitle, danceability, energy, valence);
-                    $("#track-list").append(tag);
-                });
-
-                $('#login').hide();
-                $('#loggedin').show();
-            }
-        });
+(async function() {
+    if(error) {
+        alert("There was an error during authentication");
     } else {
-        $("#login").show();
-        $("#loggedin").hide();
+        if(access_token) {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/me/top/tracks?limit=50',
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                },
+                success: function(response) {
+                    var items = response.items;
+                    items.forEach(element => {
+                        var trackId = element.uri.substring(14);
+                        var features = await getFeatures(trackId);
+    
+                        var tag = $("<tr></tr>");
+                        var songTitle = $("<td></td>").append(features.title);
+                        var danceability = $("<td></td>").append(features.danceability);
+                        var energy = $("<td></td>").append(features.energy);
+                        var valence = $("<td></td>").append(features.valence);
+                        
+                        tag.append(songTitle, danceability, energy, valence);
+                        $("#track-list").append(tag);
+                    });
+    
+                    $('#login').hide();
+                    $('#loggedin').show();
+                }
+            });
+        } else {
+            $("#login").show();
+            $("#loggedin").hide();
+        }
     }
-}
+});
--- public/song_info.js
+++ public/song_info.js
@@ -1,43 +1,39 @@
 import { access_token } from "./params.js";
 
 function getTitle(trackId) {
-    let bool = false;
-
-    $.ajax({
-        url: "https://api.spotify.com/v1/tracks/" + trackId,
-        headers: {
-            "Authorization": "Bearer " + access_token
-        },
-        success: function(response) {
-            bool = true;
-            return response.name;
-        }
+    return new Promise(function(resolve) {
+        $.ajax({
+            url: "https://api.spotify.com/v1/tracks/" + trackId,
+            headers: {
+                "Authorization": "Bearer " + access_token
+            },
+            success: function(response) {
+                resolve(response.name);
+            }
+        });
     });
-
-    if(!bool) return "";
 }
 
-function getFeatures(trackId) {
-    $.ajax({
-        url: "https://api.spotify.com/v1/audio-features/" + trackId,
-        headers: {
-            "Authorization": "Bearer " + access_token
-        },
-        success: function(response) {
-            let title = getTitle(trackId);
-            console.log(title);
-            if(title == "") return {};
-
-            return {
-                title: title,
-                danceability: response.danceability,
-                energy: response.energy,
-                valence: response.valence
-            };
-        }
+async function getFeatures(trackId) {
+    return new Promise(function(resolve) {
+        $.ajax({
+            url: "https://api.spotify.com/v1/audio-features/" + trackId,
+            headers: {
+                "Authorization": "Bearer " + access_token
+            },
+            success: function(response) {
+                let title = await getTitle(trackId);
+                if(title == undefined) return {};
+    
+                resolve({
+                    title: title,
+                    danceability: response.danceability,
+                    energy: response.energy,
+                    valence: response.valence
+                });
+            }
+        });
     });
-
-    return {};
 }
 
 export { getFeatures };
